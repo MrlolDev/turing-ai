c7ea7aa34a556bcd6514acdd2f69b728890ded3a
DisamDev <disamprivate@gmail.com>
2023-01-13 18:39:24 +0100

    updating navbar

--- app/common/components/Navbar/index.tsx
+++ app/common/components/Navbar/index.tsx
@@ -41,7 +41,7 @@ export default function Navbar() {
                 router.push("/app");
               }}
             >
-              Open Turing AI
+              Open Dashboard
             </button>
           </>
         ) : (
