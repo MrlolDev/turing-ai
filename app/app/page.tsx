de277a447becb134cfa06417f4678bd748c7e5b2
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-03-03 06:32:46 +0100

    fix

--- app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-import Navbar from "@/components/Navbar";
-import App from "@/components/pages/App";
-
-export default function Dash() {
-  return <App />;
-}
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-import Landing from "@/components/pages/Landing";
-
-export default function Home() {
-  return <Landing />;
-}
