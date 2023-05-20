a9783b13add41382cc5cc20680de58fec23823f9
MrlolDev <mrlol.yt.oficial@gmail.com>
2023-05-20 13:57:56 +0200

    feat: logout button

--- app/common/components/Pages/pay/index.tsx
+++ app/common/components/Pages/pay/index.tsx
@@ -3,7 +3,7 @@ import useUser from "../../../hooks/useUser";
 import Loading from "../Loading";
 import { useRouter } from "next/navigation";
 import PayButtons from "./PayButtons";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Image from "next/image";
 // @ts-ignore
 import sellix from "@sellix/node-sdk";
@@ -17,6 +17,7 @@ export default function PayPage() {
   const [credits, setCredits] = useState("default");
   const [servers, setServers] = useState<any>([]);
   const [selectedServer, setSelectedServer] = useState<any>(null);
+  const [openLogoutMenu, setOpenLogoutMenu] = useState(false);
 
   const handleCreditChange = (event: any) => {
     setCredits(event.target.value);
@@ -100,79 +101,61 @@ export default function PayPage() {
   }
 
   return (
-    <div className="flex flex-col items-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
-      {/*   <div className="flex flex-col items-center">
-        <h2 className="text-2xl font-bold">Features</h2>
-        <ul className="flex flex-col items-start">
-          <li>
-            <h3 className="font-semibold text-lg">Way lower cooldown</h3>
-            <p>
-              Chat with the bot for as long as you want - without being
-              interrupted by an annoying cool-down! ⏰ Your cool-down will be
-              lowered to an amazing 14 seconds, for all normal models.
-            </p>
-          </li>
-          <li>
-            <h3 className="font-semibold text-lg">No ads</h3>
-            <p>
-              No more ads! 🚫 You will no longer see any ads in the chat, and
-              you will be able to use the bot without any interruptions.
-            </p>
-          </li>
-          <li>
-            <h3 className="font-semibold text-lg">GPT-4 access</h3>
-            <p>
-              Be part of the few people that have access to GPT-4 - while still
-              being cheaper than ChatGPT Plus.
-            </p>
-          </li>
-          <li>
-            <h3 className="font-semibold text-lg">
-              Earlier access to new features
-            </h3>
-            <p>
-              As a Premium member, you get access to preview features that we
-              may add in the future, before the rest.
-            </p>
-          </li>
-          <li>
-            <h3 className="font-semibold text-lg">
-              ...a special place in our️️️ ❤
-            </h3>
-            <p>
-              Keeping this bot free is our top priority, but it wouldn&apos;t be
-              possible without supporters like you. Feel free to become one of
-              the supporters of the bot.
-            </p>
-          </li>
-        </ul>
-      </div>*/}
-      <div className="flex flex-col items-center overflow-y-auto h-[100vh] w-[100vw] py-[5vh]">
-        <h1 className="text-4xl font-semibold">Turing AI Premium</h1>
-        <SubSelector
-          sub={sub}
-          setSub={(e: any) => {
-            setSub(e);
-            setCredits("default");
-            if (e == "server") getDiscordServers();
+    <>
+      <div className="absolute top-2 right-3 cursor-pointer   z-[1000]">
+        <Image
+          alt="avatar"
+          src={profile.user.user_metadata.avatar_url}
+          width={50}
+          height={50}
+          className="rounded-full cursor-pointer"
+          onClick={() => {
+            setOpenLogoutMenu(!openLogoutMenu);
+            console.log(openLogoutMenu);
           }}
         />
-        {sub == "server" && (
-          <div className="flex flex-col items-center">
-            <select
-              className="bg-gray-900 outline-none bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100/[.2] rounded-lg shadow-lg p-2"
-              onChange={(e) => {
-                setSelectedServer(e.target.value);
+        {openLogoutMenu && (
+          <div className="absolute top-14 right-2  bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100/[.2]  rounded-md shadow-md p-2">
+            <button
+              className="text-red-500 hover:text-red-400 outline-none"
+              onClick={async () => {
+                console.log("logout");
+                await supabase.auth.signOut();
+                router.push("/");
               }}
             >
-              <option value="default">Select a server</option>
-              {servers.map((server: any) => (
-                <option value={server.id} key={server.id}>
-                  {server.name}
-                </option>
-              ))}
-            </select>
-            {/* 
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
+      <div className="flex flex-col items-center absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <div className="flex flex-col items-center overflow-y-auto h-[100vh] w-[100vw] py-[5vh]">
+          <h1 className="text-4xl font-semibold">Turing AI Premium</h1>
+          <SubSelector
+            sub={sub}
+            setSub={(e: any) => {
+              setSub(e);
+              setCredits("default");
+              if (e == "server") getDiscordServers();
+            }}
+          />
+          {sub == "server" && (
+            <div className="flex flex-col items-center">
+              <select
+                className="bg-gray-900 outline-none bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100/[.2] rounded-lg shadow-lg p-2"
+                onChange={(e) => {
+                  setSelectedServer(e.target.value);
+                }}
+              >
+                <option value="default">Select a server</option>
+                {servers.map((server: any) => (
+                  <option value={server.id} key={server.id}>
+                    {server.name}
+                  </option>
+                ))}
+              </select>
+              {/* 
             <div
               className={`flex flex-row items-center w-[600px] overflow-x-auto bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100/[.2] rounded-lg p-2 select-none`}
             >
@@ -192,29 +175,83 @@ export default function PayPage() {
               ))}
             </div>
             */}
-          </div>
-        )}
-        {/* fixed price plan card */}
-        <div className="flex flex-col  md:flex-row items-start gap-8">
-          <div className=" mt-2 flex flex-col  bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100/[.2] items-center w-[300px] h-[420px] rounded-lg shadow-lg justify-between p-2">
-            <div className="items-center flex-col flex">
-              <h2 className="text-2xl font-semibold w-fit">Fixed plan</h2>
-              <p className="w-fit">{sub == "user" ? 4.99 : 19.99}€/month</p>
-              <div>
-                <ul className="list-disc mt-2 w-fit">
+            </div>
+          )}
+          {/* fixed price plan card */}
+          <div className="flex flex-col  md:flex-row items-start gap-8">
+            <div className=" mt-2 flex flex-col  bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100/[.2] items-center w-[300px] h-[420px] rounded-lg shadow-lg justify-between p-2">
+              <div className="items-center flex-col flex">
+                <h2 className="text-2xl font-semibold w-fit">Fixed plan</h2>
+                <p className="w-fit">{sub == "user" ? 4.99 : 19.99}€/month</p>
+                <div>
+                  <ul className="list-disc mt-2 w-fit">
+                    <li
+                      title="Chat with the bot for as long as you want - without being
+              interrupted by an annoying cool-down! ⏰ Your cool-down will be
+              lowered to an amazing 14 seconds, for all normal models."
+                      className="cursor-help text-lg"
+                    >
+                      Way lower cooldown
+                    </li>
+                    <li className="cursor-help text-lg">
+                      Bigger token/character limit
+                    </li>
+                    <li className="cursor-help text-lg">No ads</li>
+                    <li className="cursor-help  text-lg">GPT-4 access</li>
+                    <li className="cursor-help  text-lg">
+                      Earlier access to new features
+                    </li>
+                    <li className="cursor-help text-lg">
+                      ...a special place in our️️️ ❤
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <PayButtons
+                handleSubscribe={(e: any) => handleSubscribe(e, "fixed")}
+              />
+            </div>
+            <div className=" mt-2 flex flex-col  bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100/[.2] items-center w-[300px] h-[420px] rounded-lg shadow-lg justify-between p-2 ">
+              <div className="items-center flex-col flex">
+                <h2 className="text-2xl font-semibold w-fit">
+                  Pay as you go Plan
+                </h2>
+                <div className="flex flex-col items-center mt-2">
+                  <input
+                    id="default-range"
+                    type="range"
+                    min={sub == "user" ? 3 : 10}
+                    max={sub == "user" ? 100 : 200}
+                    value={
+                      credits == "default" ? (sub == "user" ? 10 : 20) : credits
+                    }
+                    onChange={handleCreditChange}
+                    step="1.00"
+                    className="w-full  h-2 bg-none outline-none rounded-lg appearance-none cursor-pointer "
+                  />
+                  <p className="w-fit text-sm">
+                    {credits == "default" ? (sub == "user" ? 10 : 20) : credits}
+                    $
+                  </p>
+                </div>
+
+                <ul className="list-disc mt-2 ml-6 w-fit">
                   <li
                     title="Chat with the bot for as long as you want - without being
               interrupted by an annoying cool-down! ⏰ Your cool-down will be
               lowered to an amazing 14 seconds, for all normal models."
                     className="cursor-help text-lg"
                   >
-                    Way lower cooldown
-                  </li>
-                  <li className="cursor-help text-lg">
-                    Bigger token/character limit
+                    No cooldown
                   </li>
                   <li className="cursor-help text-lg">No ads</li>
-                  <li className="cursor-help  text-lg">GPT-4 access</li>
+                  <li className="cursor-help text-lg">
+                    No token/character limit
+                  </li>
+                  <li className="cursor-help  text-lg">
+                    GPT-4 and ChatGPT Plugins access
+                  </li>
                   <li className="cursor-help  text-lg">
                     Earlier access to new features
                   </li>
@@ -223,66 +260,14 @@ export default function PayPage() {
                   </li>
                 </ul>
               </div>
+
+              <PayButtons
+                handleSubscribe={(e: any) => handleSubscribe(e, "credits")}
+              />
             </div>
-
-            <PayButtons
-              handleSubscribe={(e: any) => handleSubscribe(e, "fixed")}
-            />
-          </div>
-          <div className=" mt-2 flex flex-col  bg-gray-500 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-30 border border-gray-100/[.2] items-center w-[300px] h-[420px] rounded-lg shadow-lg justify-between p-2 ">
-            <div className="items-center flex-col flex">
-              <h2 className="text-2xl font-semibold w-fit">
-                Pay as you go Plan
-              </h2>
-              <div className="flex flex-col items-center mt-2">
-                <input
-                  id="default-range"
-                  type="range"
-                  min={sub == "user" ? 3 : 10}
-                  max={sub == "user" ? 100 : 200}
-                  value={
-                    credits == "default" ? (sub == "user" ? 10 : 20) : credits
-                  }
-                  onChange={handleCreditChange}
-                  step="1.00"
-                  className="w-full  h-2 bg-none outline-none rounded-lg appearance-none cursor-pointer "
-                />
-                <p className="w-fit text-sm">
-                  {credits == "default" ? (sub == "user" ? 10 : 20) : credits}$
-                </p>
-              </div>
-
-              <ul className="list-disc mt-2 ml-6 w-fit">
-                <li
-                  title="Chat with the bot for as long as you want - without being
-              interrupted by an annoying cool-down! ⏰ Your cool-down will be
-              lowered to an amazing 14 seconds, for all normal models."
-                  className="cursor-help text-lg"
-                >
-                  No cooldown
-                </li>
-                <li className="cursor-help text-lg">No ads</li>
-                <li className="cursor-help text-lg">
-                  No token/character limit
-                </li>
-                <li className="cursor-help  text-lg">
-                  GPT-4 and ChatGPT Plugins access
-                </li>
-                <li className="cursor-help  text-lg">
-                  Earlier access to new features
-                </li>
-                <li className="cursor-help text-lg">
-                  ...a special place in our️️️ ❤
-                </li>
-              </ul>
-            </div>
-
-            <PayButtons
-              handleSubscribe={(e: any) => handleSubscribe(e, "credits")}
-            />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
